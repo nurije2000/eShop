8539cc6b9621ddcf5858fa0646abbed8dd0aacd3
nurije osmani <nurije.osmani2000@outlook.com>
2022-02-01 14:46:19 +0100

    Sidebar

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -107,6 +107,11 @@ export default {
   name: "admin",
   components: {
     Hero
+  },
+  methods:{
+      closeMenu(){
+          $(".page-wrapper").toggleClass("toggled");
+      }
   }
 };
 </script>
