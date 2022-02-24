2c263174a0959b81698905a30ca1f81a95fbbf10
fjolla ismaili <fjolla.ismaili.fi@gmail.com>
2022-02-25 00:35:55 +0100

    create products

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router"; 
 import jQuery from 'jquery';
-import {fb} from './firebase';
-import VueFirestore from 'vue-firestore'
+import {fb} from './firebase'
+import VueFirestore from 'vue-firestore';
+require('firebase/firestore')
 
-Vue.use(VueFirestore)
+Vue.use(VueFirestore, {
+    key: 'id',         // the name of the property. Default is '.key'.
+    enumerable: true  //  whether it is enumerable or not. Default is true.
+})
 
+
+
+  Vue.use(VueFirestore)
 
 window.$ = window.jQuery = jQuery;
 
@@ -14,21 +21,34 @@ import 'popper.js';
 import 'bootstrap'; 
 import './assets/app.scss';
 
-Vue.component('Navbar', require('./components/NavBar').default);
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  
+});
+
+window.Toast  = Toast;
+
+
+Vue.component('Navbar', require('./components/NavBar.vue').default);
 
 Vue.config.productionTip = false;
 
+let app = '';
+
 fb.auth().onAuthStateChanged(function(user) {
-
-  let app = '';
-
   if(!app){
-    new Vue({
-      router,
-      render: h => h(App)
-    }).$mount("#app");
-    
-  }
+  new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount("#app");
+
+}
 
 });
-
