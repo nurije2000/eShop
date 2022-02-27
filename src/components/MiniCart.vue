e45adb7f5259e892513c94f672ce422831c25147
gresa24 <ga47081@ubt-uni.net>
2022-02-27 12:19:23 +0100

    Checkout

--- src/components/MiniCart.vue
+++ src/components/MiniCart.vue
@@ -23,7 +23,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Continue Shopping</button>
-        <button type="button" class="btn btn-primary">Checkout</button>
+        <button type="button" class="btn btn-primary" @click="checkout">Checkout</button>
       </div>
     </div>
   </div>
@@ -36,6 +36,12 @@ export default {
    name: " MiniCart",
    props: {
        msg: String
+   }, 
+   methods: {
+       checkout () {
+           $('#miniCart').modal('hide')
+           this.$router.push ('/checkout')
+       }
    }
 }
 </script>
---
+++ src/views/Checkout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="checkout">
+
+      <Navbar> </Navbar>
+
+      <div class="container mt-5 pt-5"> 
+        <div class="row">
+
+          <div class="col-md-9">
+            <h4 class="py-4"> Checkout page </h4>
+             <ul>
+          <li v-bind="item in this.$store.state.cart" class="media">
+              <img src="item.productImage" width="80px" class="align-self-cen>" alt="">
+                   <div class="media-body">
+                  <h5 class="mt-0">  {{item.productName}}</h5>
+                  <p class="mt-0"> {{item.productPrice | currency}} </p>
+                  <p class="mt-0"> Quantity :{{item.productQuantity | currency}}</p>
+          </li>
+        </ul>
+          </div>
+          <div class="col-md-3">
+            Total Price
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
